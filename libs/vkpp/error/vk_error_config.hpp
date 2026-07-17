3bfaa38a8bb2bcae6b613cc0013c7d689154fe49
F1ST <Konrad03Nowak@gmail.com>
2026-07-17 15:28:26 +0200

    Supress clang-tidy check

--- libs/vkpp/error/vk_error_config.hpp
+++ libs/vkpp/error/vk_error_config.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#if __cpp_impl_reflection
+#if __cpp_impl_reflection // NOLINT
 #define UTILS_VK(expr, fn) ::vkpp::map_vk_error<fn>(expr)
 #else
 #define UTILS_VK(expr, fn) ::vkpp::map_vk_error(expr)
